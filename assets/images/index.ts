fb93d1cee4360e11c956bfe0a26c4286f8847e1d
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-11-24 16:04:39 +0100

    HomePage

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,5 +2,7 @@ import Lekki from './lekki.jpg'
 import Estate from './estate.jpg'
 import HeroBg from './herobg.svg'
 import HeroImg from './heroimg.svg'
+import Mockup from './mockup.jpg'
+import  Mockup2 from './mockup2.jpg'
 
-export { Lekki, Estate, HeroBg, HeroImg}
+export { Lekki, Estate, HeroBg, HeroImg, Mockup, Mockup2}
--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -1,28 +1,81 @@
-import React from 'react'
+'use client'
+import { useRef } from 'react';
+import { Mockup, HeroImg, Mockup2 } from '@/assets/images';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Features = () => {
+  const columnRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
+ const images = [Mockup, HeroImg, Mockup2, Mockup,]; 
+       // Preload images to avoid flicker
+    images.forEach(img => {
+      const p = new Image();
+      p.src = img.src;
+    });
+
+
+  useGSAP(() => {
+
+    const cards = columnRef.current?.querySelectorAll(".sticky-card");
+    if (!cards || !imageRef.current) return;
+
+    cards.forEach((card, index) => {
+      ScrollTrigger.create({
+        trigger: card,
+        start: "50% top",
+        scrub: true, 
+
+
+        onEnter: () => {
+          gsap.fromTo(imageRef.current, {opacity: 0.8}, {
+            opacity: 1,
+            backgroundImage: `url(${images[index].src})`,
+            ease: "back.inOut",
+          });
+        },
+
+        onEnterBack: () => {
+          gsap.fromTo(imageRef.current, {opacity: 0.8}, {
+            opacity: 1,
+            backgroundImage: `url(${images[index].src})`,
+            ease: "back.inOut",
+          });
+        },
+      });
+    });
+  }, []); 
+
   return (
-    <div className='flex flex-col gap-8 items-center'>
-        <h2 className='text-4xl font-semibold max-w-[500px] text-center leading-12'>
-            Powerful Features to Transform Realtors Journey
-        </h2>
-        <div className='grid grid-cols-2 gap-4 w-7xl rounded-lg p-6'>
-            <div className='flex flex-col gap-4 '>
-                <div className='w-full bg-gray-300 h-80 rounded-2xl sticky top-20 border-t-black border-t '></div>
-                <div className='w-full bg-gray-300 h-80 rounded-2xl sticky top-25 border-t-black border-t '></div>
-                <div className='w-full bg-gray-300 h-80 rounded-2xl sticky top-30 border-t-black border-t '></div>
-                <div className='w-full bg-gray-300 h-80 rounded-2xl sticky top-35 border-t-black border-t '></div>
-                <div className='w-full bg-transparent h-80 rounded-2xl sticky  top-40 '></div>
-            </div>
-            <div>
-                <div className='w-full bg-gray-300 h-150 rounded-2xl sticky top-20'></div>
-            </div>
-        </div>
-        <div className='h-400'>
+    <div className="flex flex-col gap-8 items-center">
+      <h2 className="text-4xl font-semibold max-w-[500px] text-center leading-12">
+        Powerful Features to Transform Realtors Journey
+      </h2>
 
+      <div className="grid grid-cols-2 gap-4 w-7xl rounded-lg p-6">
+
+        <div ref={columnRef} className="flex flex-col gap-4">
+          <div className="sticky-card w-full h-80 bg-gray-300 rounded-2xl sticky top-20 border-t"></div>
+          <div className="sticky-card w-full h-80 bg-gray-300 rounded-2xl sticky top-25 border-t"></div>
+          <div className="sticky-card w-full h-80 bg-gray-300 rounded-2xl sticky top-30 border-t"></div>
+          <div className="sticky-card w-full h-80 bg-gray-300 rounded-2xl sticky top-35 border-t"></div>
+          <div className="sticky-card w-full h-80 bg-transparent rounded-2xl sticky top-40"></div>
         </div>
+
+        <div>
+          <div
+            ref={imageRef}
+            className="w-full h-150 rounded-2xl sticky top-20 bg-cover bg-center"
+          >
+          </div>
+        </div>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
