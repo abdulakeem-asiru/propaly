8998e9dcd5d3a2cb6dc7694498dbeb7d706df658
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-11-25 08:11:55 +0100

    Homepage

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -5,6 +5,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
 import Image from 'next/image';
+import { Contact, Laptop2, Megaphone, Workflow } from 'lucide-react';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,7 +13,27 @@ const Features = () => {
   const columnRef = useRef<HTMLDivElement>(null);
   const imageRefs = useRef<HTMLImageElement[]>([]); // ref array for all images
   const images = [Mockup, Mockup2, Mockup, Mockup2, Mockup]; 
-
+  const featuresContent = [
+    {
+      icon : Laptop2,
+      title: "Launch a Premium Real Estate Website in Minutes",
+      description: "No developers, no coding, no stress. Realtors get a fast, beautiful, conversion-ready website instantly — live in minutes, not weeks."
+    },
+    { 
+        icon: Megaphone,
+        title: "All-in-One Property Marketing & Listing Management",
+        description: "Upload your listing once and Propaly automatically publishes, formats, and showcases it across your website and marketing channels — beautifully and professionally."},
+    {
+        icon: Contact,
+        title: "Automated Lead Capture & Follow-Up",
+        description: "Propaly turns your website into a selling machine: instant WhatsApp inquiries, smart forms, auto-response flows, and follow-up sequences so no lead goes cold."
+    },
+    { 
+        icon: Workflow,
+        title: "Automated Workflows Tailored for Realtors",
+        description: "From lead capture to closing, automate your entire real estate workflow with customizable pipelines, task reminders, and follow-up sequences designed specifically for realtors."
+    },
+  ];
   useGSAP(() => {
     const cards = columnRef.current?.querySelectorAll(".sticky-card");
     if (!cards || imageRefs.current.length === 0) return;
@@ -52,11 +73,23 @@ const Features = () => {
       <div className="grid grid-cols-2 gap-4 w-7xl rounded-lg p-6">
         {/* Sticky Cards */}
         <div ref={columnRef} className="flex flex-col gap-4">
-          <div className="sticky-card w-full h-80 bg-gray-200 border-white rounded-2xl sticky top-20 border-t"></div>
-          <div className="sticky-card w-full h-80 bg-gray-200 border-white rounded-2xl sticky top-25 border-t"></div>
-          <div className="sticky-card w-full h-80 bg-gray-200 border-white rounded-2xl sticky top-30 border-t"></div>
-          <div className="sticky-card w-full h-80 bg-gray-200 border-white rounded-2xl sticky top-35 border-t"></div>
-          <div className="sticky-card w-full h-80 bg-transparent rounded-2xl sticky top-40"></div>
+          {featuresContent.map((feature, index) => {
+          const Icon = feature.icon;
+          return (
+          <div
+            key={feature.title}
+            className={`sticky-card top-${20 + (5 * index)} items-center justify-center w-full h-80 bg-gray-100 border-white rounded-2xl sticky top-20 border-t-2`}
+          >
+            <div className='p-8'>
+              <Icon className="w-10 h-10 m-4 text-(--primary-color)" />
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
+                <p className="text-(--text-secondary)">{feature.description}</p>
+              </div>
+            </div>
+          </div>
+          )})}
+          {/* <div className="sticky-card w-full h-80 bg-transparent rounded-2xl sticky top-40"></div> */}
         </div>
 
         {/* Image container */}
