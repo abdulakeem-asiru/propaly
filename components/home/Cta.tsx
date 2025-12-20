80fc09127a34b3afbb467b5c18a5418b51ad1604
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2025-12-20 20:57:59 +0100

    Responsiveness

--- components/home/Cta.tsx
+++ components/home/Cta.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Logo from '../shared/Logo'
-import { ArrowRightSquare } from 'lucide-react'
+import { ArrowUpRight } from 'lucide-react'
 import useFadeInUp from '@/hooks/animation/useFadeInUp'
 
 const Cta = () => {
@@ -12,7 +12,8 @@ const Cta = () => {
       <div className='flex flex-col gap-4 justify-center items-center'>
         <Logo className='text-white'/>
         <h2 className="text-3xl max-w-120 text-center font-bold text-white mt-4">Ready to Grow Your Business Smarter?</h2>
-        <button className='backdrop-blur-2xl bg-linear-to-b from-[#FFC2A9] via-[#FF6347]/20 to-[#FFC2A9] inline-flex gap-2 text-lg text-white font-medium border-y justify-center border-white rounded-full p-3 md:w-full lg:w-[calc(100%-200px)]  '>Get CRM Access <ArrowRightSquare /></button>
+        <button className='backdrop-blur-2xl bg-linear-to-b from-[#FFC2A9] via-[#FF6347]/20 to-[#FFC2A9] inline-flex gap-2 md:text-lg   text-white font-medium border-y
+         justify-center border-white rounded-full p-3 md:w-full lg:w-[calc(100%-200px)]  '>Get CRM Access <ArrowUpRight /></button>
 
       </div>
     </div>
